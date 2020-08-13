06a6e0368c58dd08f5798926cfcf08c572b35430
Hazel Bracken <hazel@Hazels-MacBook-Pro.local>
2020-08-13 21:34:12 +0100

    controller tidy

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,79 +7,49 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    public function show($id)
+    public function show(Article $article)
     {
-        //show a single resource
-        $article = Article::find($id);
-
         return view('articles.show', ['article' => $article]);
     }
 
     public function index()
     {
-        // render a list
         $articles = Article::paginate(10);
         return view('articles.index', ['articles' => $articles]);
     }
 
     public function create()
     {
-        // shows a view to create a resource
         return view('articles.create');
-
     }
 
     public function store()
     {
-        // persist a new resource
+        Article::create($this->validateArticle());
 
-        request()->validate([
-            'title' => 'required',
-            'excerpt' => 'required',
-            'body' => 'required'
-        ]);
-
-        $article = new Article();
-
-        $article->title = request('title');
-        $article->excerpt = request('excerpt');
-        $article->body = request('body');
-
-        $article->save();
-
-        return redirect('/articles');
+        return redirect(route('articles.index'));
 
     }
 
-    public function edit($id)
+    public function edit(Article $article)
     {
-        //show a view to edit an existing resource
-        $article = Article::find($id);
         return view('articles.edit', compact('article'));
     }
 
-    public function update($id)
+    public function update(Article $article)
     {
 
-        request()->validate([
+        $article->update($this->validateArticle());
+
+        return redirect($article->path());
+    }
+
+    public function validateArticle(): array
+    {
+        return request()->validate([
             'title' => 'required',
             'excerpt' => 'required',
             'body' => 'required'
         ]);
-        // persist the edited resource
-        $article = Article::find($id);
-
-        $article->title = request('title');
-        $article->excerpt = request('excerpt');
-        $article->body = request('body');
-
-        $article->save();
-
-        return redirect('/articles/'. $article->id);
     }
-//
-//    public function destroy()
-//    {
-//        // delete the resource
-//    }
 }
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,7 +5,7 @@
         <div id="page" class="container">
             <div id="content">
                 @foreach($articles as $article)
-                    <h2><a href="/articles/{{ $article->id }}">{{ $article->title }}</a></h2>
+                    <h2><a href="{{ $article->path() }}">{{ $article->title }}</a></h2>
                     <h3>{{ $article->excerpt }}</h3>
 
                     <p><img src="/images/banner.jpg" alt="" class="image image-full"/></p>
