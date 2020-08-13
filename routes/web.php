270599daae1e43b22b765aeec63e98d0bbf388f4
Hazel <hazelbracken@gmail.com>
2020-08-13 21:44:03 +0100

    remove comments

--- routes/web.php
+++ routes/web.php
@@ -29,20 +29,6 @@ Route::get('/test', function (){
    ]);
 });
 
-//Route::get('/posts/{post}', function ($post) {
-//   $posts = [
-//       'my-first-post' => 'Hello, this is my first blog post',
-//       'my-second-post' => 'Now i am getting the hang of this blogging thing.'
-//   ];
-//
-//   if (! array_key_exists($post, $posts)) {
-//       abort(404, 'Sorry, that post was not found');
-//   }
-//
-//   return view('post', [
-//       'post' => $posts[$post]
-//   ]);
-//});
 Route::get('/posts/{post}', 'PostsController@show');
 
 Route::get('/contact', function (){
@@ -61,12 +47,6 @@ Route::get('/articles/create', 'ArticlesController@create');
 Route::get('/articles/{article}', 'ArticlesController@show')->name('articles.show');
 Route::get('/articles/{article}/edit', 'ArticlesController@edit');
 Route::put('/articles/{article}', 'ArticlesController@update');
-
-//Route::get('/articles', function (){
-//    return view('articles/index', [
-//        'articles' => App\Article::paginate(10)
-//    ]);
-//});
 
 
 
