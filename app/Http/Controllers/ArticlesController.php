1bcd74acc1a856898215c109df49b845ae4c3984
Hazel <hazelbracken@gmail.com>
2020-08-19 20:56:33 +0100

    eloquent 4,5

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Tag;
 use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
@@ -14,18 +15,31 @@ class ArticlesController extends Controller
 
     public function index()
     {
-        $articles = Article::paginate(10);
+        if (request('tag')) {
+            $articles = Tag::where('name', request('tag'))->firstOrFail()->articles;
+        } else {
+            $articles = Article::latest()->get();
+        }
+//        $articles = Article::paginate(10);
         return view('articles.index', ['articles' => $articles]);
     }
 
     public function create()
     {
-        return view('articles.create');
+        return view('articles.create', [
+            'tags' => Tag::all()
+        ]);
     }
 
     public function store()
     {
-        Article::create($this->validateArticle());
+        $this->validateArticle();
+
+        $article = new Article(request(['title', 'excerpt', 'body']));
+        $article->user_id = 1; //temp until auth
+        $article->save();
+
+        $article->tags()->attach(request('tags'));
 
         return redirect(route('articles.index'));
 
@@ -49,7 +63,8 @@ class ArticlesController extends Controller
         return request()->validate([
             'title' => 'required',
             'excerpt' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'tags' => 'exists:tags,id'
         ]);
     }
 }
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -4,13 +4,16 @@
     <div id="wrapper">
         <div id="page" class="container">
             <div id="content">
-                @foreach($articles as $article)
+                @forelse($articles as $article)
                     <h2><a href="{{ $article->path() }}">{{ $article->title }}</a></h2>
                     <h3>{{ $article->excerpt }}</h3>
 
                     <p><img src="/images/banner.jpg" alt="" class="image image-full"/></p>
 
-                @endforeach
+                @empty
+                    <p>No relevant articles yet</p>
+                @endforelse
+
             </div>
         </div>
 
